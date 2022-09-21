6aec2379f988373b1cbdfb54218799e18fffacf1
EtiamNullam <10875340+EtiamNullam@users.noreply.github.com>
2022-09-21 02:14:43 +0200

    Redo when position changed after undo

--- lua/gradual-undo.lua
+++ lua/gradual-undo.lua
@@ -1,3 +1,23 @@
 M = {}
 
+local function get_cursor_position()
+  return vim.fn.getpos('.')
+end
+
+function M.jump_to_last_undo_position_or_undo()
+  local cursor_position_before = get_cursor_position()
+
+  vim.api.nvim_command('undo')
+
+  local cursor_position_after = get_cursor_position()
+
+  for index = 1, #cursor_position_before, 1 do
+    if cursor_position_before[index] ~= cursor_position_after[index] then
+      vim.api.nvim_command('redo')
+
+      return
+    end
+  end
+end
+
 return M
