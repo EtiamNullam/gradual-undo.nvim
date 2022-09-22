6daa15cc043fe223e10d29ceadd1c979a3820c96
EtiamNullam <10875340+EtiamNullam@users.noreply.github.com>
2022-09-22 05:21:13 +0200

    feat: Make behavior consistent for `redo`

--- lua/gradual-undo.lua
+++ lua/gradual-undo.lua
@@ -11,14 +11,24 @@ function M.jump_to_last_undo_position_or_undo()
 
   local cursor_position_after = get_cursor_position()
 
-  for index = 1, #cursor_position_before, 1 do
-    if cursor_position_before[index] ~= cursor_position_after[index] then
-      vim.api.nvim_command('redo')
+  if not vim.deep_equal(cursor_position_before, cursor_position_after) then
+    vim.api.nvim_command('redo')
 
-      print('gradual-undo: Jumped to last undo location')
+    print('gradual-undo: Jumped to last undo location')
+  end
+end
 
-      return
-    end
+function M.jump_to_last_redo_position_or_redo()
+  local cursor_position_before = get_cursor_position()
+
+  vim.api.nvim_command('redo')
+
+  local cursor_position_after = get_cursor_position()
+
+  if not vim.deep_equal(cursor_position_before, cursor_position_after) then
+    vim.api.nvim_command('undo')
+
+    print('gradual-undo: Jumped to last redo location')
   end
 end
 
@@ -27,6 +37,7 @@ function M.setup(opts)
 
   if opts.map_default_keys == true or opts.map_default_keys == nil then
     vim.keymap.set('n', 'u', M.jump_to_last_undo_position_or_undo)
+    vim.keymap.set('n', '<c-r>', M.jump_to_last_redo_position_or_redo)
   end
 end
 
